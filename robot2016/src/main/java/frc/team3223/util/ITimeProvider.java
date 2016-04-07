978cc87cb6250721996914742ee33045cf99e848
Brayden Banks <bb010g@gmail.com>
2016-04-07 12:16:05 -0700

    Finish cleanup

--- robot2016/src/main/java/frc/team3223/util/ITimeProvider.java
+++
@@ -1,5 +0,0 @@
-package frc.team3223.util;
-
-public interface ITimeProvider {
-    long getCurrentTimeMillis();
-}
--- robot2016/src/main/java/frc/team3223/util/TimeProvider.java
+++
@@ -1,8 +0,0 @@
-package frc.team3223.util;
-
-public class TimeProvider implements ITimeProvider{
-    @Override
-    public long getCurrentTimeMillis() {
-        return System.currentTimeMillis();
-    }
-}
