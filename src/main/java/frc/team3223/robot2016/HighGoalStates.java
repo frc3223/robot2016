03cae01b67bc134c839ef5db41771a1399c7fadb
Samantha Miller <sammiller1999@hotmail.com>
2016-04-08 15:10:50 -0700

    added high goal autonomous

---
+++ src/main/java/frc/team3223/robot2016/HighGoalStates.java
@@ -0,0 +1,5 @@
+package frc.team3223.robot2016;
+
+public enum HighGoalStates {
+  DrivingBackwards, CalculateRotation, Rotating, CalculateRotationCorrection, RotateToTarget, Done;
+}
---
+++ src/main/java/frc/team3223/robot2016/ShootStateMachine.java
@@ -0,0 +1,7 @@
+package frc.team3223.robot2016;
+
+/**
+ * Created by Samantha on 4/8/2016.
+ */
+public class ShootStateMachine {
+}
