314b7c1dc119645ed46d6deca9450d255f702ce0
Samantha Miller <sammiller1999@hotmail.com>
2016-04-08 15:22:39 -0700

    added high goal auto

---
+++ robot2016/src/main/java/frc/team3223/drive/SimpleDrive.java
@@ -0,0 +1,125 @@
+package frc.team3223.drive;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import frc.team3223.robot2016.RobotConfiguration;
+
+/**
+ * silly wrapper around silly RobotDrive class.
+ * Mostly to make its safety helper shut up when it isn't being used.
+ * But also incorporates the idea of an 'equalizer' (todo: naming) that
+ * snaps the joystick inputs to the same value if they are pretty close to each other.
+ * And also incorporates the idea of a zero threshold to keep the motors
+ * from running a tiny amount when the user isn't doing anything with the joysticks
+ */
+public class SimpleDrive implements IDrive{
+    public RobotDrive drive;
+    private NetworkTable networkTable;
+
+    private double maxSpeedModifier;
+
+    /**
+     * if joystick inputs are within this distance from each other,
+     * the user intends them to be the same, so make that happen
+     */
+    private double equalizeThreshold;
+
+    /**
+     * if joystick input is within this distance to zero,
+     * the user intends it to be zero, so make that happen.
+     */
+    private double zeroThreshold;
+
+    /**
+     * true => forward means forward and backwards means backwards
+     * false => forward means backwards etc
+     */
+    private boolean normalJoystickOrientation;
+
+    private Joystick leftJoystick;
+    private Joystick rightJoystick;
+
+    public SimpleDrive(RobotConfiguration conf, NetworkTable networkTable) {
+        this.leftJoystick = conf.getLeftJoystick();
+        this.rightJoystick = conf.getRightJoystick();
+        this.networkTable = networkTable;
+        drive = new RobotDrive(
+                conf.getFrontLeftTalon(),
+                conf.getRearLeftTalon(),
+                conf.getFrontRightTalon(),
+                conf.getRearRightTalon());
+
+        setMaxSpeedModifier(0.85);
+        setEqualizeThreshold(0.5);
+        setZeroThreshold(0.2);
+        disable();
+    }
+
+    @Override
+    public void enable() {
+        drive.setSafetyEnabled(true);
+    }
+
+    @Override
+    public void disable() {
+        //drive.setSafetyEnabled(false);
+    }
+
+    public void drive() {
+        double leftValue = leftJoystick.getAxis(Joystick.AxisType.kY) * getMaxSpeedModifier();
+        double rightValue = rightJoystick.getAxis(Joystick.AxisType.kY) * getMaxSpeedModifier();
+        if(Math.abs(leftValue) < getZeroThreshold()) {
+            leftValue = 0;
+        }
+        if(Math.abs(rightValue) < getZeroThreshold()) {
+            rightValue = 0;
+        }
+        if( Math.abs(leftValue - rightValue) <= 0.5)
+        {
+            leftValue = rightValue;
+        }
+        if(!normalJoystickOrientation){
+            leftValue=-leftValue;
+            rightValue=-rightValue;
+        }
+
+        drive(leftValue, -rightValue);
+    }
+
+    public void drive(double leftValue, double rightValue) {
+        if(networkTable != null) {
+            networkTable.putNumber("left", leftValue);
+            networkTable.putNumber("right", rightValue);
+        }
+        drive.tankDrive(leftValue, rightValue, true);
+    }
+
+    public double getMaxSpeedModifier() {
+        return maxSpeedModifier;
+    }
+
+    public void setMaxSpeedModifier(double maxSpeedModifier) {
+        this.maxSpeedModifier = maxSpeedModifier;
+    }
+
+    public void toggleNormalJoystickOrientation() {
+        normalJoystickOrientation = !normalJoystickOrientation;
+    }
+
+    public double getEqualizeThreshold() {
+        return equalizeThreshold;
+    }
+
+    public void setEqualizeThreshold(double equalizeThreshold) {
+        this.equalizeThreshold = equalizeThreshold;
+    }
+
+    public double getZeroThreshold() {
+        return zeroThreshold;
+    }
+
+    public void setZeroThreshold(double zeroThreshold) {
+        this.zeroThreshold = zeroThreshold;
+    }
+}
---
+++ robot2016/src/main/java/frc/team3223/robot2016/RobotModule.java
@@ -0,0 +1,296 @@
+package frc.team3223.robot2016;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.tables.ITable;
+import edu.wpi.first.wpilibj.tables.ITableListener;
+import frc.team3223.autonomous.DriveToHighGoal;
+import frc.team3223.autonomous.IAutonomous;
+import frc.team3223.drive.*;
+import frc.team3223.navx.INavX;
+import frc.team3223.navx.NavXRegistrar;
+import frc.team3223.util.ToggleButton;
+import jaci.openrio.toast.core.ToastBootstrap;
+import jaci.openrio.toast.lib.log.Logger;
+import jaci.openrio.toast.lib.module.IterativeModule;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RobotModule extends IterativeModule implements ITableListener {
+
+    public static Logger logger;
+
+    NetworkTable networkTable;
+    DriveMode driveMode = DriveMode.SimpleTank;
+    DriveMode lastDriveMode = DriveMode.SimpleTank;
+    SimpleDrive simpleDrive;
+    PolarTankDrive ptDrive;
+    RotateToAngle rotateToAngle;
+    Map<DriveMode, IDrive> driveModes;
+    Map<AutonomousMode, IAutonomous> autonomousModes;
+    DriveToHighGoal driveToHighGoal;
+    AutonomousMode currentAutonomousMode;
+    SillyAimAssist aimAssist;
+    RobotConfiguration conf;
+    Recorder recorder;
+    Replayer replayer;
+
+    boolean inRecordingMode = false;
+    String recordingName = "recorded";
+
+    Shooter shooter;
+    ArrayList<ToggleButton> toggleButtons;
+    boolean shouldRotate = false;
+
+    double desiredHeading = 0.00;
+    private int autoBegin = 0;
+
+    @Override
+    public String getModuleName() {
+        return "robot2016";
+    }
+
+    @Override
+    public String getModuleVersion() {
+        return "0.0.1";
+    }
+
+    @Override
+    public void robotInit() {
+        logger = new Logger("robot2016", Logger.ATTR_DEFAULT);
+        networkTable = NetworkTable.getTable("SmartDashboard");
+        networkTable.addTableListener(this);
+        toggleButtons = new ArrayList<>();
+        driveModes = new HashMap<>();
+        autonomousModes = new HashMap<>();
+        currentAutonomousMode = AutonomousMode.DriveToHighGoal;
+        conf = new RobotConfiguration(networkTable);
+        recorder = new Recorder(conf);
+        replayer = new Replayer(conf);
+
+        initShooter();
+        initSimpleDrive();
+        initRotateToAngle();
+        initPolarDrive();
+        initAimAssist();
+        initDriveToHighGoal();
+
+        conf.publishJoystickConfiguration();
+    }
+
+    private void initAimAssist() {
+        aimAssist = new SillyAimAssist(conf);
+        driveModes.put(DriveMode.AimAssist, aimAssist);
+        ToggleButton toggle = conf.makeAimAssistToggle();
+        toggle.onToggleOn((j, b) -> {
+            pushDriveMode(DriveMode.AimAssist);
+        });
+        toggle.onToggleOff((j, b) -> {
+            revertDriveMode();
+        });
+        toggleButtons.add(toggle);
+    }
+
+    private void initDriveToHighGoal() {
+        this.driveToHighGoal = new DriveToHighGoal(1, ptDrive, 1);
+        driveToHighGoal.disable();
+        autonomousModes.put(AutonomousMode.DriveToHighGoal, driveToHighGoal);
+    }
+
+    private void initShooter() {
+        shooter = new Shooter(conf, networkTable);
+        networkTable.addTableListener(shooter);
+    }
+
+    private void initSimpleDrive() {
+        simpleDrive = new SimpleDrive(this.conf, networkTable);
+        driveModes.put(DriveMode.SimpleTank, simpleDrive);
+        ToggleButton toggle = conf.makeSimpleDriveResetToggle();
+        toggle.onToggleOn((j, b) -> {
+            lastDriveMode = driveMode = DriveMode.SimpleTank;
+        });
+        toggleButtons.add(toggle);
+
+        toggle = conf.makeSimpleDriveReverseToggle();
+        toggle.onToggleOn((j, b) -> {
+            simpleDrive.toggleNormalJoystickOrientation();
+        });
+        toggleButtons.add(toggle);
+    }
+
+    private void initRotateToAngle() {
+        rotateToAngle = new RotateToAngle(conf.getNavX(), simpleDrive);
+        networkTable.addTableListener(rotateToAngle);
+        toggleButtons.add(conf.makeRotateToAngleToggle()
+                .onToggleOn((j, b) -> {
+                    pushDriveMode(DriveMode.RotateToAngle);
+                })
+                .onToggleOff((j, b) -> {
+                    revertDriveMode();
+                }));
+        driveModes.put(DriveMode.RotateToAngle, rotateToAngle);
+    }
+
+    private void initPolarDrive() {
+        ptDrive = new PolarTankDrive(conf.getNavX(), this.conf, this.networkTable);
+        ptDrive.setDirectionJoystick(conf.getLeftJoystick());
+        toggleButtons.add(conf.makePolarDriveToggle()
+                .onToggleOn((j, b) -> {
+                    if(driveMode == DriveMode.PolarFCTank) {
+                        revertDriveMode();
+                    }else{
+                        pushDriveMode(DriveMode.PolarFCTank);
+                    }
+                }));
+        driveModes.put(DriveMode.PolarFCTank, ptDrive);
+    }
+
+    private void revertDriveMode() {
+        disableDriveModes();
+        driveMode = lastDriveMode;
+        driveModes.get(driveMode).enable();
+    }
+
+    private void pushDriveMode(DriveMode driveMode) {
+        disableDriveModes();
+        lastDriveMode = this.driveMode;
+        this.driveMode = driveMode;
+        driveModes.get(driveMode).enable();
+    }
+
+    private void disableDriveModes() {
+        driveModes.values().forEach(dr -> {
+            dr.disable();
+        });
+    }
+
+    private void disableAutonomousModes() {
+        autonomousModes.values().forEach(au -> {
+            au.disable();
+        });
+    }
+    public static boolean isReal() {
+        return !ToastBootstrap.isSimulation;
+    }
+
+    @Override
+    public void autonomousInit() {
+        shooter.publishValues();
+        conf.publishJoystickConfiguration();
+
+        // tell raspi to begin logging sensor data
+        networkTable.putNumber("autonomousBegin", autoBegin++);
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+        publishState();
+
+        conf.toggleButtonsPeriodic();
+        if (currentAutonomousMode == AutonomousMode.DriveToHighGoal) {
+            this.driveToHighGoal.autonomousPeriodic();
+        }
+    }
+
+    @Override public void teleopInit() {
+        conf.publishJoystickConfiguration();
+        pushDriveMode(DriveMode.SimpleTank);
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        /*publishState();
+        if(inRecordingMode) {
+            if(recorder.isRecording()) {
+                recorder.record();
+            }else{
+                conf.stopMotors();
+            }
+        }else {
+            conf.toggleButtonsPeriodic();
+            toggleButtons.forEach(tb -> tb.periodic());
+
+            shooter.teleopPeriodic();
+
+            switch(driveMode) {
+                case SimpleTank:
+                    simpleDrive.drive();
+                    break;
+                case RotateToAngle:
+                    //rotateToAngle.rotate();
+                    break;
+                case PolarFCTank:
+                    //ptDrive.driveSingleFieldCentric();
+                    break;
+                case AimAssist:
+                    //aimAssist.drive();
+                    break;
+            }
+        }*/
+        simpleDrive.drive(-conf.getLeftJoystick().getAxis(Joystick.AxisType.kY), conf.getRightJoystick().getAxis(Joystick.AxisType.kY));
+    }
+
+    @Override
+    public void testPeriodic() {
+        conf.toggleButtonsPeriodic();
+        publishState();
+    }
+
+    @Override
+    public void disabledPeriodic() {
+        conf.toggleButtonsPeriodic();
+        publishState();
+    }
+
+
+    public void publishState() {
+        if(isReal()) {
+            networkTable.putNumber("angle", conf.getNavX().getAngle());
+            networkTable.putNumber("dangle", conf.getNavX().getRate());
+            networkTable.putNumber("yaw", conf.getNavX().getYaw());
+            networkTable.putNumber("pitch", conf.getNavX().getPitch());
+            networkTable.putNumber("roll", conf.getNavX().getRoll());
+            networkTable.putNumber("accel_x", conf.getNavX().getWorldLinearAccelX());
+            networkTable.putNumber("accel_y", conf.getNavX().getWorldLinearAccelY());
+            networkTable.putNumber("accel_z", conf.getNavX().getWorldLinearAccelZ());
+            networkTable.putNumber("velocity_x", conf.getNavX().getVelocityX());
+            networkTable.putNumber("velocity_y", conf.getNavX().getVelocityY());
+            networkTable.putNumber("velocity_z", conf.getNavX().getVelocityZ());
+            networkTable.putNumber("pos_x", conf.getNavX().getDisplacementX());
+            networkTable.putNumber("pos_y", conf.getNavX().getDisplacementY());
+            networkTable.putNumber("pos_z", conf.getNavX().getDisplacementZ());
+            networkTable.putNumber("fused_heading", conf.getNavX().getFusedHeading());
+            networkTable.putNumber("shooter_pitch", conf.getShooterPitch());
+        }
+        networkTable.putString("driveMode", driveMode.toString());
+        //networkTable.putNumber("raw_shooter_angle", conf.getShooterGyro().getAngle());
+    }
+
+    @Override
+    public void valueChanged(ITable table,
+            String name, Object value, boolean isNew) {
+        System.out.println("received " + name + ": " + value);
+        switch(name) {
+            case "recordMode": {
+                inRecordingMode = (boolean) value;
+                break;
+            }
+            case "recordName": {
+                recordingName = (String) value;
+                recorder.setup(recordingName);
+                break;
+            }
+            case "recording": {
+                boolean recording = (boolean) value;
+                if(recording) {
+                    inRecordingMode = true;
+                    recorder.startRecording();
+                }
+                break;
+            }
+        }
+    }
+}
